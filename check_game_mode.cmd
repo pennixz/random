fef5a5d83a4c12f1e2328d12ce51a6ff1d1e1170
Espen <espenlillejord@gmail.com>
2018-11-22 15:30:08 +0100

    .

---
+++ check_game_mode.cmd
@@ -0,0 +1,17 @@
+@echo off
+SETLOCAL EnableExtensions
+set device_one = "Speakers"
+set device_two = "Digital Audio (S/PDIF)"
+set EXE=GameBarPresenceWriter.exe
+:start
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto FOUND
+echo Not running
+nircmd.exe setdefaultsounddevice %device_one%
+
+goto FIN
+:FOUND
+echo Running
+nircmd.exe setdefaultsounddevice %device_two%
+
+:FIN
+ENDLOCAL
